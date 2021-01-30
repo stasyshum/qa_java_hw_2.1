a8222fe7a22dad23879408654f2fea0ffe452139
Anastasia Shusmskaya <anastasiakoa@icloud.com>
2021-01-30 22:04:17 +0300

    add calculation logic

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       int account=2_000_000_000 ;
+       int transfer = 500_000_000 ;
+       int total = account+transfer;
+       System.out.println("Текущий счет!" + total);
     }
 }
